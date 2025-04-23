04802526c4a37910331e6e3810761b5ad7f7b3f9
Chong Lee <upgamesemail@gmail.com>
2025-04-23 21:17:50 +0300

    updated the colors and added a custom event that dispatches from teams_sidebar.js to it's parent element

--- script/components/teams_sidebar.js
+++ script/components/teams_sidebar.js
@@ -1,12 +1,12 @@
 
 export class TeamsSidebar extends HTMLElement{
     //html template for the teams sidebar component
-    template(data) {
+    get template() {
         return /*html*/ `
         <style>${this.style}</style>
         
         <aside class="teams-sidebar" id="teams-sidebar">
-            <button class="sidebar-button">
+            <button id="sidebar-button" class="sidebar-button">
                 <img src="../../img/side.svg" alt="teams">
             </button>
             <div class="sidebar-body">
@@ -17,7 +17,7 @@ export class TeamsSidebar extends HTMLElement{
                 </div>
                 <h3>Teams</h2>
                 <div class="teams-container" id="teams-container">
-                    <team-display></team-display>
+                    <!-- Teams will be rendered here -->
                 </div>
             </div>
             
@@ -76,9 +76,17 @@ export class TeamsSidebar extends HTMLElement{
         } 
         `;
     }
-    addTeam(){
-
+    //add a single team to the sidebar
+    addTeam(team){
+        const teamsContainer = this.shadowRoot.getElementById("teams-container");
+        const teamCard = document.createElement("team-card");
+        teamCard.setAttribute("data-json", JSON.stringify(team));
+        teamsContainer.appendChild(teamCard);
+        // Save the teams to local storage
+        this.teams.push(team);
+        localStorage.setItem("teams", JSON.stringify(this.teams));
     }
+    //render the teams to the sidebar
     renderTeams(teams){
         const teamsContainer = this.shadowRoot.getElementById("teams-container");
         teamsContainer.innerHTML = ""; // Clear the container before rendering
@@ -91,13 +99,43 @@ export class TeamsSidebar extends HTMLElement{
         
     }
 
+
     constructor(){
         super();
         this.attachShadow({mode: "open"});
+        //render the template to the shadow root
+        this.shadowRoot.innerHTML = this.template;
+        //get the teams from local storage if they exist
+        this.teams = JSON.parse(localStorage.getItem("teams")) || [];
+        //render the teams to the sidebar
+        this.renderTeams(this.teams);
 
-        this.shadowRoot.innerHTML = this.template();
-
-        this.shadowRoot.getElementById("teams-sidebar");
+        //add event listeners to the create team button
+        this.shadowRoot.getElementById("create-team").addEventListener("click", () => {
+            //get the value of the team name input field
+            //check if the team name is empty
+            const teamName = this.shadowRoot.getElementById("team-name").value;
+            if (teamName == ""){
+                alert("Please enter a team name");
+                return;
+            }
+            //check if the team name already exists¨
+            const teamExists = this.teams.some(team => team.name === teamName);
+            if (teamExists){
+                alert("Team name already exists");
+                return;
+            }
+            //create a new custom event for creating a new team
+            const event = new CustomEvent("create-team", {
+                detail: { name: teamName },
+                bubbles: true,
+                composed: true
+            });
+            //reset the team name input field
+            this.shadowRoot.getElementById("team-name").value = "";
+            //dispatch the event to the parent element (app in this case)
+            this.dispatchEvent(event);
+        });
     }
 
 }
